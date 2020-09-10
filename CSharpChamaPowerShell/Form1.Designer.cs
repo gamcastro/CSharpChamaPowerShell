d3573f13a16e3a092727616a7fca5088303d1ba8
George Castro <gamcastro@bol.com.br>
2020-09-10 11:28:23 -0300

    Adicionar arquivos de projeto.

---
+++ CSharpChamaPowerShell/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace CSharpChamaPowerShell
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCallPowerShell = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnCallPowerShell
+            // 
+            this.btnCallPowerShell.Location = new System.Drawing.Point(179, 150);
+            this.btnCallPowerShell.Name = "btnCallPowerShell";
+            this.btnCallPowerShell.Size = new System.Drawing.Size(126, 23);
+            this.btnCallPowerShell.TabIndex = 0;
+            this.btnCallPowerShell.Text = "Chama PowerShell";
+            this.btnCallPowerShell.UseVisualStyleBackColor = true;
+            this.btnCallPowerShell.Click += new System.EventHandler(this.btnCallPowerShell_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(493, 333);
+            this.Controls.Add(this.btnCallPowerShell);
+            this.Name = "Form1";
+            this.Text = "Form1";            
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCallPowerShell;
+    }
+}
+
---
+++ CSharpChamaPowerShell/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharpChamaPowerShell
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCallPowerShell_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (Process proc = new Process())
+                {
+                    string batDir = @"C:\app";
+
+                    proc.StartInfo.WorkingDirectory = batDir;
+                    proc.StartInfo.FileName = "testebat.bat";
+                    proc.StartInfo.CreateNoWindow = false;
+                    proc.Start();
+                    proc.WaitForExit();
+                    var exitCode = proc.ExitCode;
+                    MessageBox.Show("Saída do programa " + exitCode);
+
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.StackTrace.ToString());
+            }
+        }
+
+       
+    }
+}
